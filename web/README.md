963abd6e3b3fdd79a0d86633b4cabfa2fc60d6fc
MUTO Nao <n@o625.com>
2022-02-12 19:50:22 +0900

    メール飛ばす処理を実装

---
+++ web/README.md
@@ -0,0 +1,13 @@
+## 使い方
+
+.env を .env.example を参考に作成する。
+
+Gmailの場合、パスワードは [アプリ パスワードを作成、使用する](https://support.google.com/accounts/answer/185833?hl=ja) を参考に、これ専用のものを作成すると安全である。
+
+.env の作成が完了したら起動する。
+
+```
+docker-compose up
+```
+
+8089番ポートに起動する。
--- web/index.php
+++ web/index.php
@@ -1,4 +1,24 @@
 <?php
 require_once "vendor/autoload.php";
 
-print("Hello World");
+$mail_address = $_ENV['EMAIL_ADDRESS'];
+$mail_password = $_ENV['EMAIL_PASSWORD'];
+
+$mailer = new \PHPMailer\PHPMailer\PHPMailer(true);
+$mailer->CharSet = "iso-2022-jp-ms";
+$mailer->Encoding = "7bit";
+$mailer->IsSMTP();
+$mailer->Host = "smtp.gmail.com";
+$mailer->SMTPAuth = true;
+$mailer->SMTPDebug = 2;
+$mailer->SMTPSecure = "tls";
+$mailer->Port = 587;
+$mailer->Username = $mail_address;
+$mailer->Password = $mail_password;
+$mailer->setFrom($mail_address, '冷蔵庫アラートくん');
+
+$mailer->AddAddress($mail_address);
+$mailer->Subject = mb_encode_mimeheader("冷蔵庫が開いてしまっています!", 'iso-2022-jp-ms');
+$mailer->Body = mb_convert_encoding("冷蔵庫が開いてしまっています!", "iso-2022-jp-ms", "utf-8" );
+
+$mailer->Send();
